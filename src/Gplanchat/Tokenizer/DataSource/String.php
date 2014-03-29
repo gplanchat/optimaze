5d0613d796d71c0c5c36436baf80c02ee30940a4
Gregory PLANCHAT <g.planchat@gmail.com>
2014-03-29 16:23:54 +0100

    Updated PHPDoc
    Signed-off-by: Grégory PLANCHAT <g.planchat@gmail.com>

--- src/Gplanchat/Tokenizer/DataSource/String.php
+++ src/Gplanchat/Tokenizer/DataSource/String.php
@@ -1,13 +1,32 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Greg
- * Date: 17/03/14
- * Time: 12:06
+ * This file is part of gplanchat/php-javascript-tokenizer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Grégory Planchat <g.planchat@gmail.com>
+ * @licence GNU General Public Licence
+ * @package Gplanchat\Tokenizer
  */
 
 namespace Gplanchat\Tokenizer\DataSource;
 
+/**
+ * String data source
+ *
+ * @package Gplanchat\Tokenizer\DataSource
+ */
 class String
     implements DataSourceInterface
 {
@@ -37,7 +56,7 @@ class String
      * @param int $offset
      * @return string
      */
-    public function __call($length = null, $offset = null)
+    public function __invoke($length = null, $offset = null)
     {
         return $this->get($length, $offset);
     }
