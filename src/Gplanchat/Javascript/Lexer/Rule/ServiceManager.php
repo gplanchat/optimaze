03b92a8f6398cecafae08e2c851486d1af6fb7d3
Gregory PLANCHAT <g.planchat@gmail.com>
2014-03-29 20:08:40 +0100

    Updated service managers
    Signed-off-by: Grégory PLANCHAT <g.planchat@gmail.com>

--- src/Gplanchat/Javascript/Lexer/Rule/ServiceManager.php
+++ src/Gplanchat/Javascript/Lexer/Rule/ServiceManager.php
@@ -10,35 +10,35 @@ class ServiceManager
 {
     use ServiceManagerTrait;
 
-    /**
-     * @var array
-     */
-    protected $invokables = [
-        'AdditiveExpression'       => AdditiveExpression::class,
-        'AndExpression'            => AndExpression::class,
-        'ArgumentList'             => ArgumentList::class,
-        'AssignmentExpression'     => AssignmentExpression::class,
-        'BitwiseAndExpression'     => BitwiseAndExpression::class,
-        'BitwiseOrExpression'      => BitwiseOrExpression::class,
-        'BitwiseXorExpression'     => BitwiseXorExpression::class,
-        'Condition'                => Condition::class,
-        'ConditionalExpression'    => ConditionalExpression::class,
-        'Constructor'              => Constructor::class,
-        'ConstructorCall'          => ConstructorCall::class,
-        'Element'                  => Element::class,
-        'EqualityExpression'       => EqualityExpression::class,
-        'Expression'               => Expression::class,
-        'MemberExpression'         => MemberExpression::class,
-        'MultiplicativeExpression' => MultiplicativeExpression::class,
-        'OrExpression'             => OrExpression::class,
-        'ParameterList'            => ParameterList::class,
-        'PrimaryExpression'        => PrimaryExpression::class,
-        'RelationalExpression'     => RelationalExpression::class,
-        'ShiftExpression'          => ShiftExpression::class,
-        'Statement'                => Statement::class,
-        'StatementList'            => StatementList::class,
-        'UnaryExpression'          => UnaryExpression::class,
-        'VariableList'             => VariableList::class,
-        'VariableListOrExpression' => VariableListOrExpression::class
-    ];
+    public function __construct()
+    {
+        $this->invokables = [
+            'AdditiveExpression'       => AdditiveExpression::class,
+            'AndExpression'            => AndExpression::class,
+            'ArgumentList'             => ArgumentList::class,
+            'AssignmentExpression'     => AssignmentExpression::class,
+            'BitwiseAndExpression'     => BitwiseAndExpression::class,
+            'BitwiseOrExpression'      => BitwiseOrExpression::class,
+            'BitwiseXorExpression'     => BitwiseXorExpression::class,
+            'Condition'                => Condition::class,
+            'ConditionalExpression'    => ConditionalExpression::class,
+            'Constructor'              => Constructor::class,
+            'ConstructorCall'          => ConstructorCall::class,
+            'Element'                  => Element::class,
+            'EqualityExpression'       => EqualityExpression::class,
+            'Expression'               => Expression::class,
+            'MemberExpression'         => MemberExpression::class,
+            'MultiplicativeExpression' => MultiplicativeExpression::class,
+            'OrExpression'             => OrExpression::class,
+            'ParameterList'            => ParameterList::class,
+            'PrimaryExpression'        => PrimaryExpression::class,
+            'RelationalExpression'     => RelationalExpression::class,
+            'ShiftExpression'          => ShiftExpression::class,
+            'Statement'                => Statement::class,
+            'StatementList'            => StatementList::class,
+            'UnaryExpression'          => UnaryExpression::class,
+            'VariableList'             => VariableList::class,
+            'VariableListOrExpression' => VariableListOrExpression::class
+        ];
+    }
 }
