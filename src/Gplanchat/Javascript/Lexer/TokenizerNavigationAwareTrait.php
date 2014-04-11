2b35385aec0aa40393a3138c3040c127260c5ae7
Gregory PLANCHAT <g.planchat@gmail.com>
2014-04-11 02:28:46 +0200

    Updated PHPDoc
    Signed-off-by: Grégory PLANCHAT <g.planchat@gmail.com>

--- src/Gplanchat/Javascript/Lexer/TokenizerNavigationAwareTrait.php
+++ src/Gplanchat/Javascript/Lexer/TokenizerNavigationAwareTrait.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * This file is part of gplanchat/php-javascript-tokenizer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Grégory Planchat <g.planchat@gmail.com>
+ * @licence GNU General Public Licence
+ * @package Gplanchat\Tokenizer
+ */
 
 namespace Gplanchat\Javascript\Lexer;
 
@@ -6,6 +26,11 @@ use Gplanchat\Javascript\Tokenizer\TokenizerInterface;
 use Gplanchat\Javascript\Lexer\Exception\LexicalError;
 use Gplanchat\Tokenizer\Token;
 
+/**
+ * Make aware a class of token navigation in the tokenizer
+ *
+ * @package Gplanchat\Javascript\Lexer
+ */
 trait TokenizerNavigationAwareTrait
 {
     /**
