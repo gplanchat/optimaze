8a341bffe5f25d8b281df45ad7da63f1e1825c12
Gregory PLANCHAT <g.planchat@gmail.com>
2014-04-11 02:26:23 +0200

    Added LexerInterface
    Signed-off-by: Grégory PLANCHAT <g.planchat@gmail.com>

--- src/Gplanchat/Javascript/Lexer/Lexer.php
+++ src/Gplanchat/Javascript/Lexer/Lexer.php
@@ -1,9 +1,23 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Greg
- * Date: 17/03/14
- * Time: 10:01
+ * This file is part of gplanchat/php-javascript-tokenizer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Grégory Planchat <g.planchat@gmail.com>
+ * @licence GNU General Public Licence
+ * @package Gplanchat\Tokenizer
  */
 
 namespace Gplanchat\Javascript\Lexer;
@@ -11,9 +25,16 @@ namespace Gplanchat\Javascript\Lexer;
 use Gplanchat\Javascript\Lexer\TokenizerNavigationAwaretrait;
 use Gplanchat\Javascript\Lexer\Grammar\GrammarInterface;
 use Gplanchat\Javascript\Tokenizer\TokenizerInterface;
+use Gplanchat\Lexer\LexerInterface;
 use Gplanchat\ServiceManager\ServiceManagerInterface;
 
+/**
+ * Javascript lexer
+ *
+ * @package Gplanchat\Javascript\Lexer
+ */
 class Lexer
+    implements LexerInterface
 {
     use TokenizerNavigationAwaretrait;
 
---
+++ src/Gplanchat/Lexer/LexerInterface.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of gplanchat/php-javascript-tokenizer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Grégory Planchat <g.planchat@gmail.com>
+ * @licence GNU General Public Licence
+ * @package Gplanchat\Tokenizer
+ */
+
+namespace Gplanchat\Lexer;
+
+/**
+ * Interface LexerInterface
+ * Defines the way a lexer should react.
+ *
+ * @package Gplanchat\Lexer
+ */
+interface LexerInterface
+{
+    /**
+     * @param TokenizerInterface $tokenizer
+     * @return GrammarInterface
+     * @throws
+     *
+     * Program:
+     *     empty
+     *     Element Program
+     */
+    function parse(TokenizerInterface $tokenizer);
+}
