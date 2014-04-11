3bbc0eb84f437b068b287781cda9505b652e2f80
Gregory PLANCHAT <g.planchat@gmail.com>
2014-04-12 00:20:20 +0200

    Updated Rule\Element unit tests
    Signed-off-by: Grégory PLANCHAT <g.planchat@gmail.com>

--- test/Gplanchat/Javascript/Lexer/Rule/ElementTest.php
+++ test/Gplanchat/Javascript/Lexer/Rule/ElementTest.php
@@ -114,8 +114,8 @@ class ElementTest
         ];
 
         $ruleServices = [
-            ['ParameterList', Rule\ParameterList::class],
-            ['StatementList', Rule\StatementList::class],
+            ['ParameterList', new Rule\TokenSeeker(TokenizerInterface::OP_RIGHT_BRACKET, ')')],
+            ['StatementList', new Rule\TokenSeeker(TokenizerInterface::OP_RIGHT_CURLY, '}')],
         ];
 
         $grammarServices = [
@@ -154,14 +154,171 @@ class ElementTest
         ];
 
         $ruleServices = [
-            ['ParameterList', Rule\ParameterList::class],
-            ['StatementList', Rule\StatementList::class],
+            ['ParameterList', new Rule\TokenSeeker(TokenizerInterface::OP_RIGHT_BRACKET, ')')],
+            ['StatementList', new Rule\TokenSeeker(TokenizerInterface::OP_RIGHT_CURLY, '}')],
         ];
 
         $grammarServices = [
-            ['Element', Grammar\Element::class],
+            ['Element',         Grammar\Element::class],
             ['FunctionKeyword', Grammar\FunctionKeyword::class],
-            ['Identifier', Grammar\Identifier::class]
+            ['Identifier',      Grammar\Identifier::class]
+        ];
+
+        $root = $this->getRootGrammarMock();
+        $root->expects($this->at(0))
+            ->method('addChild')
+            ->with($this->isInstanceOf(Grammar\Element::class))
+        ;
+
+        $rule = new Element($this->getRuleServiceManagerMock($ruleServices),
+            $this->getGrammarServiceManagerMock($grammarServices));
+
+        $rule->parse($root, $this->getTokenizerMock($tokens));
+    }
+
+    /**
+     *
+     */
+    public function testFunctionWithMissingParameterLeftBracketLexerError()
+    {
+        $this->setExpectedException(Exception\LexicalError::class, 'Invalid expression : missing left bracket');
+
+        $tokens = [
+            [TokenizerInterface::KEYWORD_FUNCTION, 'function', null],
+            [TokenizerInterface::TOKEN_IDENTIFIER,        'a', null],
+            [TokenizerInterface::TOKEN_IDENTIFIER,        ',', null],
+            [TokenizerInterface::TOKEN_IDENTIFIER,        'b', null],
+            [TokenizerInterface::OP_RIGHT_BRACKET,        ')', null],
+            [TokenizerInterface::OP_LEFT_CURLY,           '{', null],
+            [TokenizerInterface::OP_RIGHT_CURLY,          '}', null],
+            [TokenizerInterface::TOKEN_END,              null, null]
+        ];
+
+        $ruleServices = [
+        ];
+
+        $grammarServices = [
+            ['Element',         Grammar\Element::class],
+            ['FunctionKeyword', Grammar\FunctionKeyword::class],
+            ['Identifier',      Grammar\Identifier::class]
+        ];
+
+        $root = $this->getRootGrammarMock();
+        $root->expects($this->at(0))
+            ->method('addChild')
+            ->with($this->isInstanceOf(Grammar\Element::class))
+        ;
+
+        $rule = new Element($this->getRuleServiceManagerMock($ruleServices),
+            $this->getGrammarServiceManagerMock($grammarServices));
+
+        $rule->parse($root, $this->getTokenizerMock($tokens));
+    }
+
+    /**
+     *
+     */
+    public function testFunctionWithMissingParameterRightBracketLexerError()
+    {
+        $this->setExpectedException(Exception\LexicalError::class, 'Invalid expression : missing right bracket');
+
+        $tokens = [
+            [TokenizerInterface::KEYWORD_FUNCTION, 'function', null],
+            [TokenizerInterface::OP_LEFT_BRACKET,         '(', null],
+            [TokenizerInterface::TOKEN_IDENTIFIER,        'a', null],
+            [TokenizerInterface::TOKEN_IDENTIFIER,        ',', null],
+            [TokenizerInterface::TOKEN_IDENTIFIER,        'b', null],
+            [TokenizerInterface::OP_LEFT_CURLY,           '{', null],
+            [TokenizerInterface::OP_RIGHT_CURLY,          '}', null],
+            [TokenizerInterface::TOKEN_END,              null, null]
+        ];
+
+        $ruleServices = [
+            ['ParameterList', new Rule\TokenSeeker(TokenizerInterface::OP_LEFT_CURLY, '{')],
+        ];
+
+        $grammarServices = [
+            ['Element',         Grammar\Element::class],
+            ['FunctionKeyword', Grammar\FunctionKeyword::class]
+        ];
+
+        $root = $this->getRootGrammarMock();
+        $root->expects($this->at(0))
+            ->method('addChild')
+            ->with($this->isInstanceOf(Grammar\Element::class))
+        ;
+
+        $rule = new Element($this->getRuleServiceManagerMock($ruleServices),
+            $this->getGrammarServiceManagerMock($grammarServices));
+
+        $rule->parse($root, $this->getTokenizerMock($tokens));
+    }
+
+    /**
+     *
+     */
+    public function testFunctionWithMissingBodyLeftCurlyLexerError()
+    {
+        $this->setExpectedException(Exception\LexicalError::class, 'Invalid expression : missing left curly brace');
+
+        $tokens = [
+            [TokenizerInterface::KEYWORD_FUNCTION, 'function', null],
+            [TokenizerInterface::OP_LEFT_BRACKET,         '(', null],
+            [TokenizerInterface::TOKEN_IDENTIFIER,        'a', null],
+            [TokenizerInterface::TOKEN_IDENTIFIER,        ',', null],
+            [TokenizerInterface::TOKEN_IDENTIFIER,        'b', null],
+            [TokenizerInterface::OP_RIGHT_BRACKET,        ')', null],
+            [TokenizerInterface::OP_RIGHT_CURLY,          '}', null],
+            [TokenizerInterface::TOKEN_END,              null, null]
+        ];
+
+        $ruleServices = [
+            ['ParameterList', new Rule\TokenSeeker(TokenizerInterface::OP_RIGHT_BRACKET, ')')],
+        ];
+
+        $grammarServices = [
+            ['Element',         Grammar\Element::class],
+            ['FunctionKeyword', Grammar\FunctionKeyword::class]
+        ];
+
+        $root = $this->getRootGrammarMock();
+        $root->expects($this->at(0))
+            ->method('addChild')
+            ->with($this->isInstanceOf(Grammar\Element::class))
+        ;
+
+        $rule = new Element($this->getRuleServiceManagerMock($ruleServices),
+            $this->getGrammarServiceManagerMock($grammarServices));
+
+        $rule->parse($root, $this->getTokenizerMock($tokens));
+    }
+
+    /**
+     *
+     */
+    public function testFunctionWithMissingBodyRightCurlyLexerError()
+    {
+        $this->setExpectedException(Exception\LexicalError::class, 'Invalid expression : missing right curly brace');
+
+        $tokens = [
+            [TokenizerInterface::KEYWORD_FUNCTION, 'function', null],
+            [TokenizerInterface::OP_LEFT_BRACKET,         '(', null],
+            [TokenizerInterface::TOKEN_IDENTIFIER,        'a', null],
+            [TokenizerInterface::TOKEN_IDENTIFIER,        ',', null],
+            [TokenizerInterface::TOKEN_IDENTIFIER,        'b', null],
+            [TokenizerInterface::OP_RIGHT_BRACKET,        ')', null],
+            [TokenizerInterface::OP_LEFT_CURLY,           '{', null],
+            [TokenizerInterface::TOKEN_END,              null, null]
+        ];
+
+        $ruleServices = [
+            ['ParameterList', new Rule\TokenSeeker(TokenizerInterface::OP_RIGHT_BRACKET, ')')],
+            ['StatementList', new Rule\TokenNullSeeker()]
+        ];
+
+        $grammarServices = [
+            ['Element',         Grammar\Element::class],
+            ['FunctionKeyword', Grammar\FunctionKeyword::class]
         ];
 
         $root = $this->getRootGrammarMock();
