7df4e96e19179d9af773a86331566236f1509870
Gregory PLANCHAT <g.planchat@gmail.com>
2014-04-14 19:42:18 +0200

    Added Expression rule unit tests
    Signed-off-by: Grégory PLANCHAT <g.planchat@gmail.com>

---
+++ test/Gplanchat/Javascript/Lexer/Rule/ExpressionTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * This file is part of gplanchat/php-javascript-tokenizer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Grégory Planchat <g.planchat@gmail.com>
+ * @licence GNU General Public Licence
+ * @package Gplanchat\Javascript\Lexer
+ */
+
+namespace Gplanchat\Javascript\Lexer\Rule;
+
+use Gplanchat\Javascript\Lexer\Exception;
+use Gplanchat\Lexer\Grammar;
+use Gplanchat\Javascript\Lexer\Rule;
+use Gplanchat\Javascript\Tokenizer\TokenizerInterface;
+
+/**
+ * Expression:
+ *     AssignmentExpression
+ *     AssignmentExpression , Expression
+ */
+class ExpressionTest
+    extends AbstractRuleTest
+{
+    /**
+     *
+     */
+    public function testExpression()
+    {
+        $tokens = [
+            [TokenizerInterface::TOKEN_IDENTIFIER,  'a', null],
+            [TokenizerInterface::OP_COMMA,          ',', null],
+            [TokenizerInterface::TOKEN_IDENTIFIER,  'b', null],
+            [TokenizerInterface::TOKEN_END,        null, null]
+        ];
+
+        $ruleServices = [
+            ['AssignmentExpression', new Rule\TokenSeekerIterator([
+                new Rule\TokenSeeker(TokenizerInterface::TOKEN_IDENTIFIER, 'a', true),
+                new Rule\TokenSeeker(TokenizerInterface::TOKEN_IDENTIFIER, 'b', true)
+                ])
+            ]
+        ];
+
+        $grammarServices = [
+            ['Expression', Grammar\Expression::class]
+        ];
+
+        $root = $this->getRootGrammarMock();
+        $root->expects($this->at(0))
+            ->method('addChild')
+            ->with($this->isInstanceOf(Grammar\Expression::class))
+        ;
+
+        $rule = new Expression($this->getRuleServiceManagerMock($ruleServices),
+            $this->getGrammarServiceManagerMock($grammarServices));
+
+        $rule->parse($root, $this->getTokenizerMock($tokens));
+    }
+}
