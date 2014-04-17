6e1d7f4bbd0fedc1b977035e27ccfd5c3d1473f7
Gregory PLANCHAT <g.planchat@gmail.com>
2014-04-18 00:26:14 +0200

    Added tokenizer functional tests
    Signed-off-by: Grégory PLANCHAT <g.planchat@gmail.com>

---
+++ test/Gplanchat/Javascript/Tokenizer/AbstractTokenizerTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * This file is part of gplanchat/php-javascript-tokenizer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Grégory Planchat <g.planchat@gmail.com>
+ * @licence GNU General Public Licence
+ * @package Gplanchat\Javascript\Lexer
+ */
+
+namespace Gplanchat\Javascript\Tokenizer;
+
+use Gplanchat\PHPUnit\Constraint\TokenList;
+use Gplanchat\Tokenizer\TokenizerInterface;
+use PHPUnit_Framework_TestCase as TestCase;
+use PHPUnit_Util_InvalidArgumentHelper as InvalidArgumentHelper;
+
+/**
+ * Class AbstractTokenizerTest
+ * @package Gplanchat\Javascript\Tokenizer
+ */
+abstract class AbstractTokenizerTest
+    extends TestCase
+{
+    /**
+     * Asserts the token list of a tokenizer.
+     *
+     * @param array  $expectedTokenList
+     * @param mixed  $tokenizer
+     * @param string $message
+     * @throws
+     */
+    public function assertTokenList($expectedTokenList, $tokenizer, $message = null)
+    {
+        if (!is_array($expectedTokenList)) {
+            throw InvalidArgumentHelper::factory(1, 'array');
+        }
+
+        if (!$tokenizer instanceof TokenizerInterface) {
+            throw InvalidArgumentHelper::factory(2, TokenizerInterface::class);
+        }
+
+        self::assertThat(
+            $tokenizer,
+            new TokenList($expectedTokenList),
+            $message
+        );
+    }
+
+    /**
+     * @param string $globExpression
+     * @return array
+     */
+    protected function baseDataProvider($globExpression)
+    {
+        $data = [];
+
+        $oldPath = getcwd();
+        chdir(__DIR__);
+        foreach (new \GlobIterator($globExpression) as $file) {
+            /** @var \SplFileInfo $file */
+            $baseName = $file->getBasename('.js');
+            if ($baseName === $file->getFilename()) {
+                continue;
+            }
+
+            $data[] = [
+                include $file->getPath() . DIRECTORY_SEPARATOR . $baseName . '.tok',
+                file_get_contents($file->getPathname())
+            ];
+        }
+        chdir($oldPath);
+
+        return $data;
+    }
+}
---
+++ test/Gplanchat/PHPUnit/Constraint/TokenList.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * PHPUnit
+ *
+ * Copyright (c) 2001-2014, Sebastian Bergmann <sebastian@phpunit.de>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Sebastian Bergmann nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    PHPUnit
+ * @subpackage Framework_Constraint
+ * @author     Sebastian Bergmann <sebastian@phpunit.de>
+ * @author     Bernhard Schussek <bschussek@2bepublished.at>
+ * @copyright  2001-2014 Sebastian Bergmann <sebastian@phpunit.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://www.phpunit.de/
+ * @since      File available since Release 3.0.0
+ */
+
+namespace Gplanchat\PHPUnit\Constraint;
+
+use Gplanchat\Tokenizer\Token;
+use Gplanchat\Tokenizer\TokenizerInterface;
+use Gplanchat\Javascript\Tokenizer\TokenizerInterface as JavascriptTokenizerInterface;
+use PHPUnit_Framework_AssertionFailedError as AssertionFailedError;
+
+/**
+ * Class TokenList
+ * @package Gplanchat\PHPUnit\Constraint
+ */
+class TokenList extends \PHPUnit_Framework_Constraint
+{
+    /**
+     * @var array
+     */
+    protected $tokenList = null;
+
+    /**
+     * @var int
+     */
+    protected $tokenIndex = 0;
+
+    /**
+     * @var string
+     */
+    protected $errorMessage = null;
+
+    /**
+     * @param array $expectedTokenList
+     * @throws
+     */
+    public function __construct($expectedTokenList)
+    {
+        parent::__construct();
+
+        $this->tokenList = new \SplQueue();
+        foreach ($expectedTokenList as $i => $tokenSpec) {
+            if (!is_array($tokenSpec)) {
+                throw new AssertionFailedError(sprintf('Invalid expected token list format at index %d, should be array.', $i));
+            }
+
+            if (!isset($tokenSpec[0]) || !isset($tokenSpec[1])) {
+                throw new AssertionFailedError(sprintf('Invalid expected token list format at index %d.', $i));
+            }
+
+            if (isset($tokenSpec[2])) {
+                $token = new Token($tokenSpec[0], $tokenSpec[1], 0, 0, 0, $tokenSpec[2]);
+            } else {
+                $token = new Token($tokenSpec[0], $tokenSpec[1], 0, 0, 0);
+            }
+            $this->tokenList->enqueue($token);
+        }
+    }
+
+    /**
+     * Evaluates the constraint for parameter $other. Returns true if the
+     * constraint is met, false otherwise.
+     *
+     * @param  mixed $other Value or object to evaluate.
+     * @return bool
+     * @throws
+     */
+    protected function matches($other)
+    {
+        $this->errorMessage = null;
+
+        if (!$other instanceof TokenizerInterface) {
+            return false;
+        }
+
+        /** @var TokenizerInterface $other */
+        $this->tokenList->rewind();
+        $other->rewind();
+        $i = 0;
+        foreach ($other as $token) {
+            $i++;
+            if (!$this->tokenList->valid()) {
+                if ($this->tokenList->count() > $i) {
+                    $this->errorMessage = sprintf('Token count does not match.');
+                    return false;
+                }
+                break;
+            }
+
+            /** @var Token $expectedToken */
+            $expectedToken = $this->tokenList->current();
+
+            /** @var Token $token */
+            if ($token->getType() !== $expectedToken->getType()) {
+                $re = new \ReflectionClass(JavascriptTokenizerInterface::class);
+                $constants = $re->getConstants();
+
+                $this->errorMessage = sprintf(
+                    'token type should match at index %d. Was %s, expected %s.',
+                    $i, array_search($token->getType(), $constants), array_search($expectedToken->getType(), $constants)
+                );
+                return false;
+            }
+
+            if ($token->getValue() !== $expectedToken->getValue()) {
+                $this->errorMessage = sprintf(
+                    'Token value does not match at index %d. Was %s, expected %s.',
+                    $i, $token->getValue(), $expectedToken->getValue()
+                );
+                return false;
+            }
+
+            if ($token->getAssignOperator() !== $expectedToken->getAssignOperator()) {
+                $this->errorMessage = sprintf(
+                    'Token assign operator does not match at index %d. Was %s, expected %s.',
+                    $i, $token->getAssignOperator(), $expectedToken->getAssignOperator()
+                );
+                return false;
+            }
+
+            $this->tokenList->next();
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns the description of the failure
+     *
+     * The beginning of failure messages is "Failed asserting that" in most
+     * cases. This method should return the second part of that sentence.
+     *
+     * @param  mixed  $other Evaluated value or object.
+     * @return string
+     */
+    protected function failureDescription($other)
+    {
+        return $this->errorMessage;
+    }
+
+    /**
+     * Returns a string representation of the constraint.
+     *
+     * @return string
+     */
+    public function toString()
+    {
+        return implode('', $this->tokenList);
+    }
+}
